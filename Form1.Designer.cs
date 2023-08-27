b299b3e739bd7527e2799f14256f3ac124799f46
Mathew Elston <elstonmathew@yahoo.com>
2023-08-27 14:05:27 -0700

    added checkboxes and add on click

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,21 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.AddButton = new System.Windows.Forms.Button();
+            this.ItemInput = new System.Windows.Forms.TextBox();
+            this.ToDoList = new System.Windows.Forms.CheckedListBox();
             this.SuspendLayout();
+            // 
+            // AddButton
+            // 
+            this.AddButton.Location = new System.Drawing.Point(416, 326);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(97, 20);
+            this.AddButton.TabIndex = 0;
+            this.AddButton.Text = "Add Item";
+            this.AddButton.UseVisualStyleBackColor = true;
+            this.AddButton.Click += new System.EventHandler(this.AddButton_Click);
+            // 
+            // ItemInput
+            // 
+            this.ItemInput.Location = new System.Drawing.Point(283, 326);
+            this.ItemInput.Name = "ItemInput";
+            this.ItemInput.Size = new System.Drawing.Size(127, 20);
+            this.ItemInput.TabIndex = 1;
+            this.ItemInput.TextChanged += new System.EventHandler(this.ItemInput_TextChanged);
+            // 
+            // ToDoList
+            // 
+            this.ToDoList.FormattingEnabled = true;
+            this.ToDoList.Location = new System.Drawing.Point(275, 159);
+            this.ToDoList.Name = "ToDoList";
+            this.ToDoList.Size = new System.Drawing.Size(237, 109);
+            this.ToDoList.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ToDoList);
+            this.Controls.Add(this.ItemInput);
+            this.Controls.Add(this.AddButton);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button AddButton;
+        private System.Windows.Forms.TextBox ItemInput;
+        private System.Windows.Forms.CheckedListBox ToDoList;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -5,24 +5,37 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        String UserInput = "";
         public Form1()
         {
             InitializeComponent();
-            titleLabel.Text = "To Do List";
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
 
         }
+
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
         }
 
-        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+
+
+        private void AddButton_Click(object sender, EventArgs e)
         {
+            ToDoList.Text = UserInput;
+            Console.WriteLine(UserInput);
+            ToDoList.Items.Add(UserInput);
+
+            ItemInput.Clear();
+
+
+
+        }
+
+        private void ItemInput_TextChanged(object sender, EventArgs e)
+        {
+            UserInput = ((TextBox)sender).Text;
+            Console.WriteLine(UserInput);
 
         }
     }
